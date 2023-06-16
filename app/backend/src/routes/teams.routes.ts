619cbcdb132d4dee01227615acf7ee485c35aec3
Karina Gonzalez <karinagonzalez99@gmail.com>
2023-06-16 17:26:35 -0300

    Requisito 5

--- app/backend/src/routes/teams.routes.ts
+++ app/backend/src/routes/teams.routes.ts
@@ -1,9 +1,9 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import Team from '../database/models/Team';
 
 const router = Router();
 
-router.get('/', async (_req, res) => {
+router.get('/', async (_req: Request, res: Response) => {
   try {
     const teams = await Team.findAll();
 
@@ -15,6 +15,25 @@ router.get('/', async (_req, res) => {
     res.status(200).json(response);
   } catch (error) {
     res.status(500).json({ error: 'Erro ao buscar os times' });
+  }
+});
+
+router.get('/:id', async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const team = await Team.findByPk(id);
+
+    if (!team) {
+      return res.status(404).json({ message: 'Time não encontrado' });
+    }
+
+    res.status(200).json({
+      id: team.id,
+      teamName: team.teamName,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Erro ao buscar o time' });
   }
 });
 
