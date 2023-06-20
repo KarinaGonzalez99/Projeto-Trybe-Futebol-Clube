9c323dd7619b202d0d09633abdc86c3a7c16f688
Karina Gonzalez <karinagonzalez99@gmail.com>
2023-06-20 18:31:21 -0300

    Requisito 16

--- app/backend/src/routes/match.routes.ts
+++ app/backend/src/routes/match.routes.ts
@@ -5,7 +5,17 @@ const router = Router();
 
 router.get('/', async (req: Request, res: Response) => {
   try {
-    const matches = await Match.findAll({ include: [{ all: true }] });
+    const { inProgress } = req.query;
+
+    let matches;
+    if (inProgress === 'true') {
+      matches = await Match.findAll({ where: { inProgress: true }, include: [{ all: true }] });
+    } else if (inProgress === 'false') {
+      matches = await Match.findAll({ where: { inProgress: false }, include: [{ all: true }] });
+    } else {
+      matches = await Match.findAll({ include: [{ all: true }] });
+    }
+
     return res.status(200).json(matches);
   } catch (error) {
     console.error(error);
